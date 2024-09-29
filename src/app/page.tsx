41480594c449c7a5af91cf0b348b24115c4a1973
al-shaimon <alshaimon968@gmail.com>
2024-09-30 01:04:22 +0600

    news feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "@/components/Navbar/Navbar";
-import Post from "@/app/(home)/@Post/page";
-import NewsFeed from "@/app/(home)/@NewsFeed/page";
+import Post from "@/app/[home]/@Post/page";
+import NewsFeed from "@/app/[home]/@NewsFeed/NewsFeed";
 
 export default function Home() {
   return (
