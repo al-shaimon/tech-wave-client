40a66a08bbda11e539f2279496e10f23e6a69031
al-shaimon <alshaimon968@gmail.com>
2024-10-01 04:51:44 +0600

    rich text editor and auth

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "@/components/Navbar/Navbar";
 import Post from "@/app/(WithCommonLayout)/(home)/[home]/@Post/page";
 import NewsFeed from "@/app/(WithCommonLayout)/(home)/[home]/@NewsFeed/NewsFeed";
 
